dd6c892c882424a2ba982d25b4c61605e94f816b
auwase <alineuwase250@gmail.com>
2024-03-17 14:26:14 +0100

    amelioration projet macro

--- Part2SerieTNew.R
+++ Part2SerieTNew.R
@@ -1,6 +1,15 @@
 # PARTIE SERIE_TEMPORELLE
-
+install.packages("tseries")
+library(tseries)
+library(readxl)
+library(astsa)
+library(ggfortify)
+library(forecast)
+library(fpp2)
+library(seasonal)
 library(dplyr)
+library(urca)
+library(xts)
 # MODELE ALLEMAGNE
 data_All <- read_excel("data_All.xlsx")
 
@@ -34,3 +43,63 @@ data_USA<-mutate(data_USA,PIB_mond2 =lag(PIB_mond2,default=0))
 data_USA<- mutate(data_USA,covid_dummy =ifelse(Date>=Date_covid , 1,0))
 
 model_USA<- lm(BC~Croissance_PIB  +PIB_mond2 + covid_dummy , data= data_USA)
+#################################### USA ################################################
+## SEASONNLITE ET RUPTURE
+head(data_USA)
+acf(data_USA)
+
+## VALEURS ABBERANTES 
+boxplot(data_USA[c("Croissance_PIB", "PIB_mond2")], 
+        main = "Boxplot pour Croissance PIB et PIB mond2",
+        names = c("Croissance PIB", "PIB mond2"),
+        las = 2, # Oriente les étiquettes perpendiculairement à l'axe
+        col = c("lightblue", "lightgreen"))
+
+## RACINE UNITAIRE (STATIONNALITE OU NON STATIONNALITE)
+##Création d'objet xts en excluant la colonne de date du data frame et en utilisant cette colonne comme index
+data_USA <- xts(data_USA[, -which(names(data_USA) == "Date")], order.by = data_USA$Date)
+##data_USA_log <- log(data_USA)
+head(data_USA)
+
+# Application du test ADF
+adf_test_USA <- lapply(data_USA[, c("BC", "Croissance_PIB", "PIB_mond2")], function(x) adf.test(x, alternative = "stationary"))
+
+# Application du test PP
+pp_test_USA <- lapply(data_USA[, c("BC", "Croissance_PIB", "PIB_mond2")], function(x) pp.test(x))
+
+# Application du test KPSS
+kpss_test_USA <- lapply(data_USA[, c("BC", "Croissance_PIB", "PIB_mond2")], function(x) kpss.test(x))
+
+
+
+
+
+
+#################################### ALLEMAGNE ################################################
+## SEASONNLITE ET RUPTURE
+head(data_All)
+acf(data_All)
+
+## VALEURS ABBERANTES 
+boxplot(data_All[c("Croissance_PIB", "PIB_mond1")], 
+        main = "Boxplot pour Croissance PIB et PIB mond1",
+        names = c("Croissance PIB", "PIB mond2"),
+        las = 2, # Oriente les étiquettes perpendiculairement à l'axe
+        col = c("lightblue", "lightgreen"))
+
+## RACINE UNITAIRE (STATIONNALITE OU NON STATIONNALITE)
+##Création d'objet xts en excluant la colonne de date du data frame et en utilisant cette colonne comme index
+data_All <- xts(data_All[, -which(names(data_All) == "Date")], order.by = data_All$Date)
+##data_USA_log <- log(data_USA)
+head(data_All)
+
+# Application du test ADF
+adf_test_All <- lapply(data_All[, c("BC", "Croissance_PIB", "PIB_mond1")], function(x) adf.test(x, alternative = "stationary"))
+
+# Application du test PP
+pp_test_All <- lapply(data_All[, c("BC", "Croissance_PIB", "PIB_mond1")], function(x) pp.test(x))
+
+# Application du test KPSS
+
+kpss_test_All <- lapply(data_USA[, c("BC", "Croissance_PIB", "PIB_mond1")], function(x) kpss.test(x))
+getwd()
